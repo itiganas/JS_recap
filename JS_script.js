867b8acca61fd0351286d87c2f031cc37115de8d
Ina Tiganas <inna.tiganas@gmail.com>
2024-04-01 18:46:23 +0200

    fixing the array

--- JS_script.js
+++ JS_script.js
@@ -1,35 +1,32 @@
-Data types in JS: 
 
-// Numbers:
-let length = 16;
-let weight = 7.5;
+window.onload = function myFunction() {
 
-// Strings:
-let color = "Yellow";
-let lastName = "Johnson";
+    let color = "Yellow";
+    let lastName = "Johnson";
+    document.getElementById("strings").innerHTML = "Hello World";
 
 // Booleans:
-let x = true;
-let y = false;
-
-//Undefined:
-let x;
+    let x = true;
+    let y = false;
+    document.getElementById("booleans").innerHTML = x + "<br>" + y;
 
 // Object:
-const person = {firstName:"John", lastName:"Doe"};
+    const person = {firstName: "John", lastName: "Doe"};
+    document.getElementById("object").innerHTML = person.firstName + "<br>" + person.lastName;
 
-// Array object:
-const cars = ["Saab", "Volvo", "BMW"];
+    // Array object:
+    const cars = ["Saab", "Volvo", "BMW", "Ferarri", "Masseratti"];
+    document.getElementById("myArray").innerHTML = cars.slice(1,4);
+
 
 // Date object:
-const date = new Date("2022-03-25");
+    const date = new Date("2022-03-25");
+    document.getElementById("date").innerHTML = date();
+
+};
 
 
-Objects
-
-const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 
 
-person.firstname;
-person[firstname];
+
 
